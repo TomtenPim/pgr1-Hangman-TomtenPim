cb5a5442ab16eecc8a1dc233ef82fae81ef1e595
TomtenPim <77719357+TomtenPim@users.noreply.github.com>
2022-02-01 14:57:12 +0100

    really beagan coding

--- src/hangman.java
+++ src/hangman.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class hangman {
+    public static void main(String[] args) {
+
+        String word = "TEST";
+        int antGiss = Integer.parseInt(JOptionPane.showInputDialog("Hur många fel tillåter du?"));
+        boolean allCorrect = false;
+
+        for(allCorrect = false;;){
+            String guess = JOptionPane.showInputDialog("Gissa en bokstav");
+            if(guess == word)
+
+        }
+
+    }
 }
