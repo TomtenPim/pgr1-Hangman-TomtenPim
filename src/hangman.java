dd2e96e05eff6e284faf4a9c01023769daaac842
TomtenPim <77719357+TomtenPim@users.noreply.github.com>
2022-02-01 21:49:34 +0100

    Made an end

--- src/hangman.java
+++ src/hangman.java
@@ -5,18 +5,23 @@ public class hangman {
 
         String word = "TEST";
         int antGiss = Integer.parseInt(JOptionPane.showInputDialog("Hur många fel tillåter du?"));
-        boolean allCorrect = false;
+        int allCorrect = 0;
         int antFel = 0;
 
-        for(allCorrect = false;;){
+        while (allCorrect < 1 && antFel < antGiss + 1){
+            System.out.println(antFel + " " + antGiss + " " + allCorrect + " " + word);
             String guess = JOptionPane.showInputDialog("Gissa en bokstav");
-            if(guess == word){
-                allCorrect = true;
+
+            if(guess == "TEST"){
+                allCorrect++;
+                System.out.println("win");
             }
             else {
                 antFel++;
             }
         }
+        if (allCorrect < 1)
+        System.out.println("You lost :(");
 
     }
 }
